59bc83cf61981961ae12e82b10f00ac8bc2bc6fe
Prashant Nigam <coolking9911@gmail.com>
2023-07-21 23:46:29 +0530

    updated links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,9 +6,9 @@ export default function Contact() {
     <>
     <p className='contact'>Connect with me Here!</p>
     <div className='faItems'>
-        <a className="fa fa-linkedin" href='https://www.google.com/' alt='linkedin'></a>
-        <a className="fa fa-envelope" href='https://www.google.com/' alt='Email'></a>
-        <a className="fa fa-whatsapp" href='https://www.google.com/' alt='Whatsapp'></a>
+        <a className="fa fa-linkedin" href='https://www.linkedin.com/in/prashant-singh-nigam/' alt='linkedin'></a>
+        <a className="fa fa-envelope" href='mailto:nigamp72@gmail.com' alt='Email'></a>
+        <a className="fa fa-whatsapp" href='https://wa.me/qr/ZVFTULQ5OUEBL1' alt='Whatsapp'></a>
     </div>
     </>
   )
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,9 +6,9 @@ export default function Nav() {
   return (
     <>
     <nav className='navbar'>
-        <Link to='/about'>About Me</Link>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contact</Link>
+        <Link className='navitem' to='/about'>About Me</Link>
+        <Link className='navitem' to='/'>Home</Link>
+        <Link className='navitem' to='/contact'>Contact</Link>
     </nav>
     <Outlet/>
     </>
